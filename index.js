7fa951ae56a626c826e38437235830116c8ab699
Ksena <oksishukh@yahoo.com>
2017-12-17 18:34:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-theBeatlesPlay(musicians, instruments); 
+theBeatlesPlay(musicians, instruments);
 
 
 function theBeatlesPlay( musicians, instruments){
