669ff8229c6a41eebee79185b02f049983a66c7e
Ksena <oksishukh@yahoo.com>
2017-12-17 18:26:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ function johnLennonFacts(arr) {
 }
  johnLennonFacts(facts);
 
- function iLoveTheBeatles(number) { 
-   
- let newArray = []; 
+ function iLoveTheBeatles(number) {
+
+ let newArray = [];
   do {
       newArray.push(`I love the Beatles!`); //this will happen one time, whether or not number meets condition below
       number++; //increment number of times string above is pushed into newArray
- } while (number < 15); //(that number subracted from 15 will be the output number of times the string is logged); 
+ } while (number < 15); //(that number subracted from 15 will be the output number of times the string is logged);
 
  return newArray; // this needs to be outside the loop, otherwise only first item will be returned
  } //end of function
