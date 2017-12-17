31118edd69e42819e0d050350840ddc6c7212ce6
Ksena <oksishukh@yahoo.com>
2017-12-17 18:34:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ theBeatlesPlay(musicians, instruments);
 
 function theBeatlesPlay( musicians, instruments){
   var newArr = []
-  for(var i = 0; i < newArr.length; i++){
+  for(var i = 0; i < musicians.length; i++){
     newArr.push(musicians[i] + plays + instruments[i] );
 
   }
